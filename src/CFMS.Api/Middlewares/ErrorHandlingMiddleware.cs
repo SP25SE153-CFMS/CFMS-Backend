bd8e790025250a4f7f709a3e34aa03edf9819341
DuongTruong <truongdinhdongduong@gmail.com>
2025-02-06 05:13:00 +0700

    add middleware, behavior, event

---
+++ src/CFMS.Api/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using CFMS.Application.Events;
+
+public class ErrorHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly EventQueue _eventQueue;
+
+    public ErrorHandlingMiddleware(RequestDelegate next, EventQueue eventQueue)
+    {
+        _next = next;
+        _eventQueue = eventQueue;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        await _next(context);
+
+        if (_eventQueue.HasErrors())
+        {
+            var error = _eventQueue.PopError();
+            context.Response.StatusCode = 400;
+            await context.Response.WriteAsJsonAsync(new { error });
+        }
+    }
+}
--- src/CFMS.Api/Program.cs
+++ src/CFMS.Api/Program.cs
@@ -1,4 +1,9 @@
 ﻿using CFMS.Api.Extensions;
+using CFMS.Application.Behaviors;
+using CFMS.Application.Events;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +12,10 @@ builder.Services.AddControllers();
 builder.Services.AddCorsPolicy();
 builder.Services.AddSwaggerDocumentation();
 
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(EventQueueBehavior<,>));
+builder.Services.AddSingleton<EventQueue>();
+builder.Services.AddMediatRServices();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -14,6 +23,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerDocumentation();
 }
 
+app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseCorsPolicy();  
 app.UseAuthorization();
 
