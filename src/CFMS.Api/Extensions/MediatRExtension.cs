ab47ae78c4444fb7d460fe992d26ff6e94cdde2a
DuongTruong <truongdinhdongduong@gmail.com>
2025-02-06 05:13:00 +0700

    add middleware, bahavior, event

---
+++ src/CFMS.Api/Extensions/MediatRExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CFMS.Api.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMediatRServices(this IServiceCollection services)
+        {
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+            });
+
+            return services;
+        }
+    }
+}
---
+++ src/CFMS.Application/Behaviors/EventQueueBehavior.cs
@@ -0,0 +1,30 @@
+﻿using CFMS.Application.Events;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFMS.Application.Behaviors
+{
+    public class EventQueueBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly EventQueue _eventQueue;
+
+        public EventQueueBehavior(EventQueue eventQueue)
+        {
+            _eventQueue = eventQueue;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (_eventQueue.HasErrors())
+            {
+                throw new Exception(_eventQueue.PopError());
+            }
+
+            return await next();
+        }
+    }
+}
