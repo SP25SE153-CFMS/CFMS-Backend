0bcc4f431c03b5c4d2a6843c37fcceda10901f85
DuongTruong <truongdinhdongduong@gmail.com>
2025-03-09 22:03:15 +0700

    feat: add save-changes-async

---
+++ src/CFMS.Domain/Interfaces/ISoftDelete.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFMS.Domain.Interfaces
+{
+    public interface ISoftDelete
+    {
+        [DefaultValue(false)]
+        public bool IsDeleted { get; set; }
+
+        public DateTimeOffset? DeletedWhen { get; set; }
+    }
+}
