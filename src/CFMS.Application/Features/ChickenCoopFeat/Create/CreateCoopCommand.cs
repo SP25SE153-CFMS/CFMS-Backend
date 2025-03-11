640148c9adc187f188593eef005638868380587f
handgun <DungPHASE172122@fpt.edu.vn>
2025-03-11 21:54:14 +0700

    feat: create delete update getcoop getcoops

---
+++ src/CFMS.Application/Features/ChickenCoopFeat/Create/CreateCoopCommand.cs
@@ -0,0 +1,9 @@
+﻿using CFMS.Application.Common;
+using MediatR;
+
+namespace CFMS.Application.Features.ChickenCoopFeat.Create
+{
+    public class CreateCoopCommand : IRequest<BaseResponse<bool>>
+    {
+    }
+}
