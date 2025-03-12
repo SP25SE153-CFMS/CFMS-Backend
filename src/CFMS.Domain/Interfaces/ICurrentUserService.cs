30fa6e3c402c5ce909a4305fd0728f5bec41f7f8
DuongTruong <truongdinhdongduong@gmail.com>
2025-03-13 00:58:22 +0700

    fix: adjust current-user-service

--- src/CFMS.Domain/Interfaces/ICurrentUserService.cs
+++ src/CFMS.Domain/Interfaces/ICurrentUserService.cs
@@ -1,9 +1,10 @@
-namespace CFMS.Domain.Interfaces
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+namespace CFMS.Domain.Interfaces
 {
     public interface ICurrentUserService
     {
