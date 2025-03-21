9931db1d529154dbd46cf6e342a6168f93212286
handgun <DungPHASE172122@fpt.edu.vn>
2025-03-21 12:18:00 +0700

    feat: add chickendetail

---
+++ src/CFMS.Application/Features/ChickenFeat/UpdateChickenDetail/UpdateChickenDetailCommand.cs
@@ -0,0 +1,27 @@
+﻿using CFMS.Application.Common;
+using MediatR;
+
+namespace CFMS.Application.Features.ChickenFeat.UpdateChickenDetail
+{
+    public class UpdateChickenDetailCommand : IRequest<BaseResponse<bool>>
+    {
+        public UpdateChickenDetailCommand(Guid chickenDetailId, Guid? chickenId, decimal? weight, int? quantity, int? gender)
+        {
+            ChickenDetailId = chickenDetailId;
+            ChickenId = chickenId;
+            Weight = weight;
+            Quantity = quantity;
+            Gender = gender;
+        }
+
+        public Guid ChickenDetailId { get; set; }
+
+        public Guid? ChickenId { get; set; }
+
+        public decimal? Weight { get; set; }
+
+        public int? Quantity { get; set; }
+
+        public int? Gender { get; set; }
+    }
+}
