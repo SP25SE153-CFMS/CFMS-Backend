bd84bde625e82b9e6a39201b5ea48bfbf4519c69
handgun <DungPHASE172122@fpt.edu.vn>
2025-03-22 12:50:18 +0700

    feat: add chicken and growthstage to batch

---
+++ src/CFMS.Application/Features/ChickenBatchFeat/AddChicken/AddChickenCommand.cs
@@ -0,0 +1,18 @@
+﻿using CFMS.Application.Common;
+using MediatR;
+
+namespace CFMS.Application.Features.ChickenBatchFeat.AddChicken
+{
+    public class AddChickenCommand : IRequest<BaseResponse<bool>>
+    {
+        public AddChickenCommand(Guid chickenId, Guid chickenBatchId)
+        {
+            ChickenId = chickenId;
+            ChickenBatchId = chickenBatchId;
+        }
+
+        public Guid ChickenId { get; set; }
+
+        public Guid ChickenBatchId { get; set; }
+    }
+}
---
+++ src/CFMS.Application/Features/ChickenBatchFeat/UpdateGrowthBatch/UpdateGrowthBatchCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using CFMS.Application.Common;
+using CFMS.Domain.Interfaces;
+using MediatR;
+
+namespace CFMS.Application.Features.ChickenBatchFeat.UpdateGrowthBatch
+{
+    public class UpdateGrowthBatchCommandHandler : IRequestHandler<UpdateGrowthBatchCommand, BaseResponse<bool>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpdateGrowthBatchCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<BaseResponse<bool>> Handle(UpdateGrowthBatchCommand request, CancellationToken cancellationToken)
+        {
+            var existGrowthBatch = _unitOfWork.GrowthBatchRepository.Get(filter: gb => gb.GrowthBatchId.Equals(request.GrowthBatchId) && gb.IsDeleted == false).FirstOrDefault();
+            if (existGrowthBatch == null)
+            {
+                return BaseResponse<bool>.FailureResponse(message: "Giai đoạn phát triển không tồn tại");
+            }
+
+            try
+            {
+                existGrowthBatch.StartDate = request.StartDate;
+                existGrowthBatch.EndDate = request.EndDate;
+                existGrowthBatch.AvgWeight = request.AvgWeight;
+                existGrowthBatch.MortalityRate = request.MortalityRate;
+                existGrowthBatch.Note = request.Note;
+                existGrowthBatch.Status = request.Status;
+
+                _unitOfWork.GrowthBatchRepository.Update(existGrowthBatch);
+                var result = await _unitOfWork.SaveChangesAsync();
+                if (result > 0)
+                {
+                    return BaseResponse<bool>.SuccessResponse(message: "Cập nhật thành công");
+                }
+                return BaseResponse<bool>.FailureResponse(message: "Cập nhật không thành công");
+            }
+            catch (Exception ex)
+            {
+                return BaseResponse<bool>.FailureResponse(message: "Có lỗi xảy ra");
+            }
+        }
+    }
+}
