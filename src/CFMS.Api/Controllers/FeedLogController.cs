631e2557cef3dba21decc09a32f3d25d70c59d05
handgun <DungPHASE172122@fpt.edu.vn>
2025-03-23 23:55:32 +0700

    feat: crud feedlog

---
+++ src/CFMS.Api/Controllers/FeedLogController.cs
@@ -0,0 +1,52 @@
+﻿using CFMS.Application.Features.FeedLogFeat.Create;
+using CFMS.Application.Features.FeedLogFeat.Delete;
+using CFMS.Application.Features.FeedLogFeat.GetFeedLog;
+using CFMS.Application.Features.FeedLogFeat.GetFeedLogs;
+using CFMS.Application.Features.FeedLogFeat.Update;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CFMS.Api.Controllers
+{
+    public class FeedLogController : BaseController
+    {
+        public FeedLogController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await Send(new GetFeedLogsQuery());
+            return result;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var result = await Send(new GetFeedLogQuery(id));
+            return result;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateFeedLogCommand command)
+        {
+            var result = await Send(command);
+            return result;
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateFeedLogCommand command)
+        {
+            var result = await Send(command);
+            return result;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var result = await Send(new DeleteFeedLogCommand(id));
+            return result;
+        }
+    }
+}
---
+++ src/CFMS.Application/Features/FeedLogFeat/Create/CreateFeedLogCommand.cs
@@ -0,0 +1,30 @@
+﻿using CFMS.Application.Common;
+using MediatR;
+
+namespace CFMS.Application.Features.FeedLogFeat.Create
+{
+    public class CreateFeedLogCommand : IRequest<BaseResponse<bool>>
+    {
+        public CreateFeedLogCommand(Guid? chickenBatchId, DateTime? feedingDate, decimal? actualFeedAmount, Guid? unitId, Guid? taskId, string? note)
+        {
+            ChickenBatchId = chickenBatchId;
+            FeedingDate = feedingDate;
+            ActualFeedAmount = actualFeedAmount;
+            UnitId = unitId;
+            TaskId = taskId;
+            Note = note;
+        }
+
+        public Guid? ChickenBatchId { get; set; }
+
+        public DateTime? FeedingDate { get; set; }
+
+        public decimal? ActualFeedAmount { get; set; }
+
+        public Guid? UnitId { get; set; }
+
+        public Guid? TaskId { get; set; }
+
+        public string? Note { get; set; }
+    }
+}
