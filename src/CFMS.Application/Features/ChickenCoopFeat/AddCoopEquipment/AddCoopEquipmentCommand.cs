57c93194e913e4ac244eda80883402f663599983
handgun <DungPHASE172122@fpt.edu.vn>
2025-03-25 15:11:40 +0700

    feat: crud coopequipment

---
+++ src/CFMS.Application/Features/ChickenCoopFeat/AddCoopEquipment/AddCoopEquipmentCommand.cs
@@ -0,0 +1,26 @@
+﻿using CFMS.Application.Common;
+using MediatR;
+
+namespace CFMS.Application.Features.ChickenCoopFeat.AddCoopEquipment
+{
+    public class AddCoopEquipmentCommand : IRequest<BaseResponse<bool>>
+    {
+        public Guid? ChickenCoopId { get; set; }
+
+        public Guid? EquipmentId { get; set; }
+
+        public int? Quantity { get; set; }
+
+        public DateTime? AssignedDate { get; set; }
+
+        public DateTime? LastMaintenanceDate { get; set; }
+
+        public DateTime? NextMaintenanceDate { get; set; }
+
+        public int MaintenanceInterval { get; set; }
+
+        public int? Status { get; set; }
+
+        public string? Note { get; set; }
+    }
+}
---
+++ src/CFMS.Application/Features/ChickenCoopFeat/UpdateCoopEquipment/UpdateCoopEquipmentCommand.cs
@@ -0,0 +1,42 @@
+﻿using CFMS.Application.Common;
+using MediatR;
+
+namespace CFMS.Application.Features.ChickenCoopFeat.UpdateCoopEquipment
+{
+    public class UpdateCoopEquipmentCommand : IRequest<BaseResponse<bool>>
+    {
+        public UpdateCoopEquipmentCommand(Guid coopEquipmentId, Guid? chickenCoopId, Guid? equipmentId, int? quantity, DateTime? assignedDate, DateTime? lastMaintenanceDate, DateTime? nextMaintenanceDate, int maintenanceInterval, int? status, string? note)
+        {
+            CoopEquipmentId = coopEquipmentId;
+            ChickenCoopId = chickenCoopId;
+            EquipmentId = equipmentId;
+            Quantity = quantity;
+            AssignedDate = assignedDate;
+            LastMaintenanceDate = lastMaintenanceDate;
+            NextMaintenanceDate = nextMaintenanceDate;
+            MaintenanceInterval = maintenanceInterval;
+            Status = status;
+            Note = note;
+        }
+
+        public Guid CoopEquipmentId { get; set; }
+
+        public Guid? ChickenCoopId { get; set; }
+
+        public Guid? EquipmentId { get; set; }
+
+        public int? Quantity { get; set; }
+
+        public DateTime? AssignedDate { get; set; }
+
+        public DateTime? LastMaintenanceDate { get; set; }
+
+        public DateTime? NextMaintenanceDate { get; set; }
+
+        public int MaintenanceInterval { get; set; }
+
+        public int? Status { get; set; }
+
+        public string? Note { get; set; }
+    }
+}
