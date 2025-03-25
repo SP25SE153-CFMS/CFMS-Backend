1e0590d20c5138628dd7736b69adbc00c67d721e
handgun <DungPHASE172122@fpt.edu.vn>
2025-03-26 00:52:40 +0700

    feat: crud employee

---
+++ src/CFMS.Application/Features/FarmFeat/DeleteFarmEmployee/DeleteFarmEmployeeCommand.cs
@@ -0,0 +1,12 @@
+﻿using CFMS.Application.Common;
+using MediatR;
+
+namespace CFMS.Application.Features.FarmFeat.DeleteFarmEmployee
+{
+    public class DeleteFarmEmployeeCommand : IRequest<BaseResponse<bool>>
+    {
+        public Guid? FarmId { get; set; }
+
+        public Guid? FarmEmployeeId { get; set; }
+    }
+}
