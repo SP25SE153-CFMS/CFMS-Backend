b23a96b849a4be52a1698ab688ed78711109d4b3
handgun <DungPHASE172122@fpt.edu.vn>
2025-03-31 17:09:26 +0700

    fix: getcoopbyid

--- src/CFMS.Application/Features/ChickenCoopFeat/GetCoop/GetCoopQueryHandler.cs
+++ src/CFMS.Application/Features/ChickenCoopFeat/GetCoop/GetCoopQueryHandler.cs
@@ -16,7 +16,7 @@ namespace CFMS.Application.Features.ChickenCoopFeat.GetCoop
 
         public async Task<BaseResponse<ChickenCoop>> Handle(GetCoopQuery request, CancellationToken cancellationToken)
         {
-            var existCoop = _unitOfWork.ChickenCoopRepository.Get(filter: c => c.ChickenCoopId.Equals(request.Id) && c.IsDeleted == false, includeProperties: "ChickenBatches").FirstOrDefault();
+            var existCoop = _unitOfWork.ChickenCoopRepository.Get(filter: c => c.ChickenCoopId.Equals(request.Id) && c.IsDeleted == false, includeProperties: [c => c.ChickenBatches, c => c.CoopEquipments]).FirstOrDefault();
             if (existCoop == null)
             {
                 return BaseResponse<ChickenCoop>.FailureResponse(message: "Chuồng gà không tồn tại");
