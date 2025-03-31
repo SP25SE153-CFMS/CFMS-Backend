639b7f1ba651b95e81f1146d7f20ae9f0124cb8c
DuongTruong <truongdinhdongduong@gmail.com>
2025-03-31 23:54:20 +0700

    fix: fix supplier

--- src/CFMS.Application/Features/RequestFeat/CreateReceipt/CreateReceiptCommandHandler.cs
+++ src/CFMS.Application/Features/RequestFeat/CreateReceipt/CreateReceiptCommandHandler.cs
@@ -9,6 +9,7 @@ namespace CFMS.Application.Features.RequestFeat.CreateReceipt
     using global::CFMS.Application.Common;
     using global::CFMS.Application.Features.InventoryReceipts.Commands;
     using global::CFMS.Domain.Entities;
+    using global::CFMS.Domain.Enums.Types;
     using global::CFMS.Domain.Interfaces;
     using MediatR;
     using System;
@@ -45,6 +46,13 @@ namespace CFMS.Application.Features.RequestFeat.CreateReceipt
                     if (existRequest.Status.Equals(0))
                     {
                         return BaseResponse<bool>.FailureResponse("Phiếu yêu cầu đang chờ duyệt");
+                    }
+
+                    var existReceiptType = _unitOfWork.SubCategoryRepository.Get(filter: x => x.SubCategoryId.Equals(request.ReceiptTypeId) && x.IsDeleted == false).FirstOrDefault();
+
+                    if (existReceiptType == null)
+                    {
+                        return BaseResponse<bool>.FailureResponse("Loại phiếu không tồn tại");
                     }
 
 
