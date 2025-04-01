f34a0e74d9f89d9285ae177e388e9484b2b26136
handgun <DungPHASE172122@fpt.edu.vn>
2025-04-01 13:59:36 +0700

    fix: create growthstage

--- src/CFMS.Application/Features/GrowthStageFeat/Create/CreateStageCommand.cs
+++ src/CFMS.Application/Features/GrowthStageFeat/Create/CreateStageCommand.cs
@@ -5,16 +5,19 @@ namespace CFMS.Application.Features.GrowthStageFeat.Create
 {
     public class CreateStageCommand : IRequest<BaseResponse<bool>>
     {
-        public CreateStageCommand(string? stageName, Guid? chickenType, int? minAgeWeek, int? maxAgeWeek, string? description)
+        public CreateStageCommand(string? stageName, Guid? chickenType, int? minAgeWeek, int? maxAgeWeek, string? description, string stageCode)
         {
             StageName = stageName;
             ChickenType = chickenType;
             MinAgeWeek = minAgeWeek;
             MaxAgeWeek = maxAgeWeek;
             Description = description;
+            StageCode = stageCode;
         }
 
         public string? StageName { get; set; }
+
+        public string StageCode { get; set; }
 
         public Guid? ChickenType { get; set; }
 
