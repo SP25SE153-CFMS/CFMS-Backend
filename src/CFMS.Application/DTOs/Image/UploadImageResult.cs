2b410238dcd8dc0fccfbada25e8f4dc89503aca9
DuongTruong <truongdinhdongduong@gmail.com>
2025-04-05 06:06:11 +0700

    feat: add upload image to ggdrive

---
+++ src/CFMS.Application/DTOs/Image/UploadImageResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFMS.Application.DTOs.Image
+{
+    public class UploadImageResult
+    {
+        public string FileId { get; set; }
+        public string FileUrl { get; set; }
+    }
+}
---
+++ src/CFMS.Application/Services/Impl/GoogleDriveService.cs
@@ -0,0 +1,82 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Microsoft.Extensions.Configuration;
+using File = Google.Apis.Drive.v3.Data.File;
+
+public class GoogleDriveService : IGoogleDriveService
+{
+    private readonly DriveService _driveService;
+    private readonly string _folderId;
+
+    public GoogleDriveService(IConfiguration configuration)
+    {
+        var credentialPath = configuration["GoogleDrive:ServiceAccountKeyPath"];
+        _folderId = configuration["GoogleDrive:FolderId"];
+
+        GoogleCredential credential;
+        using (var stream = new FileStream(credentialPath, FileMode.Open, FileAccess.Read))
+        {
+            credential = GoogleCredential.FromStream(stream)
+                .CreateScoped(DriveService.Scope.DriveFile);
+        }
+
+        _driveService = new DriveService(new BaseClientService.Initializer()
+        {
+            HttpClientInitializer = credential,
+            ApplicationName = "CFMS-DriveImage"
+        });
+    }
+
+    public async Task<string> UploadFileAsync(string filePath, string contentType, string fileName, string folderName)
+    {
+        var request = _driveService.Files.List();
+        request.Q = $"name = '{folderName}' and mimeType = 'application/vnd.google-apps.folder' and trashed = false";
+        var result = await request.ExecuteAsync();
+
+        string folderId;
+        if (result.Files.Count > 0)
+        {
+            folderId = result.Files[0].Id;
+        }
+        else
+        {
+            var folderMetadata = new File
+            {
+                Name = folderName,
+                MimeType = "application/vnd.google-apps.folder"
+            };
+            var folder = await _driveService.Files.Create(folderMetadata).ExecuteAsync();
+            folderId = folder.Id;
+        }
+
+        var fileMeta = new File
+        {
+            Name = fileName,
+            Parents = new List<string> { folderId }
+        };
+
+        FilesResource.CreateMediaUpload requestUpload;
+
+        using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+        {
+            requestUpload = _driveService.Files.Create(fileMeta, stream, contentType);
+            requestUpload.Fields = "id, webViewLink, webContentLink";
+
+            await requestUpload.UploadAsync();
+        }
+
+        var file = requestUpload.ResponseBody;
+
+        var permissionForAll = new Permission
+        {
+            Type = "anyone",
+            Role = "reader"
+        };
+        await _driveService.Permissions.Create(permissionForAll, file.Id).ExecuteAsync();
+
+        return file.Id;
+    }
+}
