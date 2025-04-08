5879e9f77bf438ca896a9084a9611a7246b1edd5
DuongTruong <truongdinhdongduong@gmail.com>
2025-04-08 14:36:11 +0700

    feat: add get-subs-by-cate-type

---
+++ src/CFMS.Application/Features/CategoryFeat/GetSubsByType/GetSubsByTypeQuery.cs
@@ -0,0 +1,21 @@
+﻿using CFMS.Application.Common;
+using CFMS.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFMS.Application.Features.CategoryFeat.GetSubsByCode
+{
+    public class GetSubsByTypeQuery : IRequest<BaseResponse<IEnumerable<SubCategory>>>
+    {
+        public string CategoryType { get; set; }
+
+        public GetSubsByTypeQuery(string categoryType)
+        {
+            CategoryType = categoryType;
+        }
+    }
+}
