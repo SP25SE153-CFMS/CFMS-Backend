c1c71b1734a00394cf3850fa8e0659934b35823e
DuongTruong <truongdinhdongduong@gmail.com>
2025-04-18 07:40:47 +0700

    feat: get sub-cate-by-name

--- src/CFMS.Application/Features/CategoryFeat/GetSubByName/GetSubByNameQueryHandler.cs
+++ src/CFMS.Application/Features/CategoryFeat/GetSubByName/GetSubByNameQueryHandler.cs
@@ -1,6 +1,6 @@
 ﻿using AutoMapper;
 using CFMS.Application.Common;
-using CFMS.Application.Features.CategoryFeat.GetCategory;
+using CFMS.Application.Features.CategoryFeat.GetSub;
 using CFMS.Domain.Entities;
 using CFMS.Domain.Interfaces;
 using MediatR;
@@ -10,22 +10,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CFMS.Application.Features.CategoryFeat.GetSub
+namespace CFMS.Application.Features.CategoryFeat.GetSubByName
 {
-    public class GetSubQueryHandler : IRequestHandler<GetSubQuery, BaseResponse<SubCategory>>
+    public class GetSubByNameQueryHandler : IRequestHandler<GetSubByNameQuery, BaseResponse<SubCategory>>
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
 
-        public GetSubQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        public GetSubByNameQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
         }
 
-        public async Task<BaseResponse<SubCategory>> Handle(GetSubQuery request, CancellationToken cancellationToken)
+        public async Task<BaseResponse<SubCategory>> Handle(GetSubByNameQuery request, CancellationToken cancellationToken)
         {
-            var existSubCategory = _unitOfWork.SubCategoryRepository.Get(filter: c => c.SubCategoryId.Equals(request.SubCategoryId) && c.IsDeleted == false).FirstOrDefault();
+            var existSubCategory = _unitOfWork.SubCategoryRepository.Get(filter: c => c.SubCategoryId.Equals(request.SubCategoryName) && c.IsDeleted == false).FirstOrDefault();
             if (existSubCategory == null)
             {
                 return BaseResponse<SubCategory>.FailureResponse(message: "SubCategory không tồn tại");
