bd654bd580f29e28a380488a381d5ec0c5e71a87
handgun <DungPHASE172122@fpt.edu.vn>
2025-04-21 15:51:04 +0700

    fix create nutrition plan

--- src/CFMS.Application/Features/NutritionPlanFeat/Create/CreateNutritionPlanCommand.cs
+++ src/CFMS.Application/Features/NutritionPlanFeat/Create/CreateNutritionPlanCommand.cs
@@ -7,17 +7,20 @@ namespace CFMS.Application.Features.NutritionPlanFeat.Create
 {
     public class CreateNutritionPlanCommand : IRequest<BaseResponse<bool>>
     {
-        public CreateNutritionPlanCommand(string? name, string? description, List<NutritionPlanDetailDto> nutritionPlanDetails, List<FeedSessionRequest>? feedSessions)
+        public CreateNutritionPlanCommand(string? name, string? description, List<NutritionPlanDetailDto> nutritionPlanDetails, List<FeedSessionRequest>? feedSessions, Guid farmId)
         {
             Name = name;
             Description = description;
             NutritionPlanDetails = nutritionPlanDetails;
             FeedSessions = feedSessions;
+            FarmId = farmId;
         }
 
         public string? Name { get; set; }
 
         public string? Description { get; set; }
+
+        public Guid FarmId { get; set; }
 
         public List<NutritionPlanDetailDto>? NutritionPlanDetails { get; set; }
 
