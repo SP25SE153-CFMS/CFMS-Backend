148727c41758efbc5e01880073e5ae8067c68093
DuongTruong <truongdinhdongduong@gmail.com>
2025-04-22 11:57:33 +0700

    fix: fix update farm-employee with phone-number and mail

---
+++ src/CFMS.Application/Features/FarmFeat/GetFarmEmployee/GetFarmEmployeeQuery.cs
@@ -0,0 +1,22 @@
+﻿using CFMS.Application.Common;
+using CFMS.Application.DTOs.FarmEmployee;
+using CFMS.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFMS.Application.Features.FarmFeat.GetFarmEmployee
+{
+    public class GetFarmEmployeeQuery : IRequest<BaseResponse<FarmEmployeeResponse>>
+    {
+        public GetFarmEmployeeQuery(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/CFMS.Application/Features/FarmFeat/GetFarmEmployee/GetFarmEmployeeQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using CFMS.Application.Common;
+using CFMS.Application.DTOs.FarmEmployee;
+using CFMS.Application.Features.FarmFeat.GetFarm;
+using CFMS.Domain.Entities;
+using CFMS.Domain.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFMS.Application.Features.FarmFeat.GetFarmEmployee
+{
+    public class GetFarmEmployeeQueryHandler : IRequestHandler<GetFarmEmployeeQuery, BaseResponse<FarmEmployeeResponse>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetFarmEmployeeQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<BaseResponse<FarmEmployeeResponse>> Handle(GetFarmEmployeeQuery request, CancellationToken cancellationToken)
+        {
+            var existFarmEmployee = _unitOfWork.FarmEmployeeRepository.Get(filter: f => f.FarmEmployeeId.Equals(request.Id) && f.IsDeleted == false, includeProperties: [e => e.User]).FirstOrDefault();
+            if (existFarmEmployee == null)
+            {
+                return BaseResponse<FarmEmployeeResponse>.FailureResponse(message: "Nhân viên không làm việc trong trang trại này");
+            }
+
+            var employee = _mapper.Map<FarmEmployeeResponse>(existFarmEmployee);
+            employee.User.Mail = existFarmEmployee.Mail;
+            employee.User.PhoneNumber = existFarmEmployee.PhoneNumber;
+
+            return BaseResponse<FarmEmployeeResponse>.SuccessResponse(data: employee);
+        }
+    }
+}
