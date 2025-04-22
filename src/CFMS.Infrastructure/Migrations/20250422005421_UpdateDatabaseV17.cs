60af5715a9d1e5226d293a6ac122f08c73ee674f
DuongTruong <truongdinhdongduong@gmail.com>
2025-04-22 07:55:21 +0700

    config db

---
+++ src/CFMS.Infrastructure/Migrations/20250422005421_UpdateDatabaseV17.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CFMS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDatabaseV17 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BatchNumber",
+                table: "InventoryReceiptDetail");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BatchNumber",
+                table: "InventoryRequest",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BatchNumber",
+                table: "InventoryRequest");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BatchNumber",
+                table: "InventoryReceiptDetail",
+                type: "integer",
+                nullable: true);
+        }
+    }
+}
