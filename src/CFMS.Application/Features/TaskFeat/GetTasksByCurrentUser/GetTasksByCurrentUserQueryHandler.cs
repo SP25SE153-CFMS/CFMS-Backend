ebf3774fc6d4f2b2217d476db5f69086d9775448
DuongTruong <truongdinhdongduong@gmail.com>
2025-04-25 01:00:36 +0700

    fix: fix get task by current user

--- src/CFMS.Application/Features/TaskFeat/GetTasksByCurrentUser/GetTasksByCurrentUserQueryHandler.cs
+++ src/CFMS.Application/Features/TaskFeat/GetTasksByCurrentUser/GetTasksByCurrentUserQueryHandler.cs
@@ -5,6 +5,7 @@ using CFMS.Application.Features.TaskFeat.GetTasks;
 using CFMS.Application.Services.Impl;
 using CFMS.Domain.Interfaces;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,15 +32,59 @@ namespace CFMS.Application.Features.TaskFeat.GetTasksByCurrentUser
             var currentUser = _currentUserService.GetUserId();
             Guid userId = Guid.Parse(_currentUserService.GetUserId());
 
-            var existTasks = _unitOfWork.TaskRepository.Get(filter: f => (f.FarmId.ToString().Equals(request.FarmId) || f.Assignments.Select(x => x.AssignedToId).Contains(userId)) && f.IsDeleted == false,
-                  includeProperties: [
-                    x => x.Assignments,                      
-                    x => x.TaskType,
-                    x => x.ShiftSchedules,
-                    x => x.TaskHarvests,
-                    x => x.TaskResources,
-                    x => x.TaskLocations
-                      ]);
+            var existTasks = _unitOfWork.TaskRepository.GetIncludeMultiLayer(filter: f => (f.FarmId.ToString().Equals(request.FarmId) && f.Assignments.Select(x => x.AssignedToId).Contains(userId)) && f.IsDeleted == false,
+               include: q => q
+                    .Include(t => t.Assignments)
+                        .ThenInclude(s => s.AssignedTo)
+                    .Include(t => t.TaskType)
+                    .Include(t => t.ShiftSchedules)
+                        .ThenInclude(s => s.Shift)
+                    .Include(t => t.TaskHarvests)
+                    .Include(t => t.TaskResources)
+                        .ThenInclude(s => s.Resource)
+                            .ThenInclude(r => r.Food)
+                    .Include(t => t.TaskResources)
+                        .ThenInclude(s => s.Resource)
+                            .ThenInclude(r => r.Medicine)
+                    .Include(t => t.TaskResources)
+                        .ThenInclude(s => s.Resource)
+                            .ThenInclude(r => r.Equipment)
+                    .Include(t => t.TaskResources)
+                        .ThenInclude(s => s.ResourceType)
+                    .Include(t => t.TaskResources)
+                        .ThenInclude(s => s.Resource)
+                            .ThenInclude(s => s.Unit)
+                    .Include(t => t.TaskResources)
+                        .ThenInclude(s => s.Resource)
+                            .ThenInclude(s => s.Package)
+                    .Include(t => t.TaskResources)
+                        .ThenInclude(s => s.Unit)
+                    .Include(t => t.TaskLocations)
+                        .ThenInclude(s => s.Location)
+                    .Include(t => t.TaskLocations)
+                        .ThenInclude(s => s.LocationNavigation)
+                    .Include(t => t.TaskResources)
+                        .ThenInclude(s => s.Resource)
+                            .ThenInclude(z => z.Unit)
+                    .Include(t => t.TaskResources)
+                        .ThenInclude(s => s.Resource)
+                            .ThenInclude(z => z.Package)
+                    .Include(t => t.FeedLogs)
+                        .ThenInclude(s => s.Resource)
+                            .ThenInclude(z => z.Food)
+                    .Include(t => t.FeedLogs)
+                        .ThenInclude(s => s.Resource)
+                            .ThenInclude(z => z.Equipment)
+                    .Include(t => t.FeedLogs)
+                        .ThenInclude(s => s.Resource)
+                            .ThenInclude(z => z.Medicine)
+                    .Include(t => t.FeedLogs)
+                        .ThenInclude(s => s.Resource)
+                            .ThenInclude(z => z.Unit)
+                    .Include(t => t.FeedLogs)
+                        .ThenInclude(s => s.Resource)
+                            .ThenInclude(z => z.Package)
+                    ).ToList();
 
             return BaseResponse<IEnumerable<TaskResponse>>.SuccessResponse(data: _mapper.Map<IEnumerable<TaskResponse>>(existTasks));
         }
