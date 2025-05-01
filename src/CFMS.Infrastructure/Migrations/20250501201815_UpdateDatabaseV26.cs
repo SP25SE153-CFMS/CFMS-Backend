f5aa075a5647a807f45e0e2157b0d9db554aed1a
DuongTruong <truongdinhdongduong@gmail.com>
2025-05-02 03:20:08 +0700

    config db

---
+++ src/CFMS.Infrastructure/Migrations/20250501201815_UpdateDatabaseV26.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CFMS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDatabaseV26 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "FarmId",
+                table: "InventoryReceipt",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FarmId",
+                table: "InventoryReceipt");
+        }
+    }
+}
