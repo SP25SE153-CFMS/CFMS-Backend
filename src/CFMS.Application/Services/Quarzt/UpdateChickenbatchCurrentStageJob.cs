ef8a0ca7f014c01e69f107c0afad2e6acf24fd15
DuongTruong <truongdinhdongduong@gmail.com>
2025-05-05 01:50:07 +0700

    chore: adjust message and date

--- src/CFMS.Application/Services/Quarzt/UpdateChickenbatchCurrentStageJob.cs
+++ src/CFMS.Application/Services/Quarzt/UpdateChickenbatchCurrentStageJob.cs
@@ -29,7 +29,7 @@ namespace CFMS.Application.Services.Quartz
                 var systemId = _unitOfWork.UserRepository.Get(filter: u => u.SystemRole == -1).FirstOrDefault()?.UserId;
                 _currentUserService.SetSystemId(systemId.Value);
 
-                var today = DateTime.Now.Date;
+                var today = DateTime.Now.ToLocalTime().Date;
 
                 var chickenBatches = _unitOfWork.ChickenBatchRepository.Get(
                     filter: cb => !cb.IsDeleted && cb.StartDate != null,
