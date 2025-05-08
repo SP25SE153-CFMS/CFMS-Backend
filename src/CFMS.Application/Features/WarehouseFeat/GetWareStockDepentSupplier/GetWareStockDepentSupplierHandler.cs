5eb7abdc85f2ca61fb1ce34ab7e503e86ac40eb9
DuongTruong <truongdinhdongduong@gmail.com>
2025-05-08 23:53:23 +0700

    fix: fix get warestock depen supplier after adding resource to supplier

--- src/CFMS.Application/Features/WarehouseFeat/GetWareStockDepentSupplier/GetWareStockDepentSupplierHandler.cs
+++ src/CFMS.Application/Features/WarehouseFeat/GetWareStockDepentSupplier/GetWareStockDepentSupplierHandler.cs
@@ -308,6 +308,242 @@ namespace CFMS.Application.Features.WarehouseFeat.GetWareStockDepentSupplier
                     }
                 }
 
+                if (resourceSuppliers.Any())
+                {
+                    var quantity = 0;
+
+                    if (existResourceType.SubCategoryName == "food" && !resource.WareStocks.Any(x => x.SupplierId != null))
+                    {
+                        var defaultResponse = (WareStockResponseBase)new WareStockFoodResponse
+                        {
+                            ResourceId = resource.ResourceId,
+                            FoodId = resource.FoodId ?? Guid.Empty,
+                            FoodCode = resource.Food?.FoodCode,
+                            FoodName = resource.Food?.FoodName,
+                            Note = resource.Food?.Note,
+                            ProductionDate = resource.Food?.ProductionDate,
+                            ExpiryDate = resource.Food?.ExpiryDate,
+                            SpecQuantity = (resource?.PackageSize > 0)
+                                                ? $"{(int)(quantity / resource.PackageSize)} {package.SubCategoryName}" +
+                                                  $"{(quantity % resource.PackageSize > 0 ? $" (lẻ {quantity % resource.PackageSize} {unit.SubCategoryName})" : "")}"
+                                                : $"{(int)quantity} {unit.SubCategoryName}",
+                            UnitSpecification = $"{resource.PackageSize} {unit.SubCategoryName}/{package.SubCategoryName}",
+                            CurrentSupplierId = null,
+                            CurrentSupplierName = "Không rõ nguồn gốc",
+                            SupplierName = resourceSuppliers.Count > 0 ? $"Có {resourceSuppliers.Count} nhà cung cấp" : "Không rõ nguồn gốc",
+                            SuppliersName = resourceSuppliers
+                                    .Where(rs => rs.Supplier != null)
+                                    .GroupBy(rs => rs?.Supplier?.SupplierId)
+                                    .Select(g =>
+                                    {
+                                        var first = g.First();
+                                        var matchedResourceSupplier = g.FirstOrDefault(x =>
+                                            x.ResourceId == resource?.ResourceId &&
+                                            x.SupplierId == x.Supplier?.SupplierId);
+
+                                        return new SupplierResponse
+                                        {
+                                            SupplierId = first.Supplier?.SupplierId,
+                                            SupplierCode = first.Supplier?.SupplierCode,
+                                            SupplierName = first.Supplier?.SupplierName,
+                                            ResourceSupplierId = matchedResourceSupplier?.ResourceSupplierId
+                                        };
+                                    })
+                                    .ToList()
+                        };
+
+                        return new List<WareStockResponseBase> { defaultResponse };
+                    }
+
+                    if (existResourceType.SubCategoryName == "equipment" && !resource.WareStocks.Any(x => x.SupplierId != null))
+                    {
+                        var existMaterial = _unitOfWork.SubCategoryRepository.Get(filter: f => f.SubCategoryId.Equals(resource.Equipment.MaterialId) && f.IsDeleted == false).FirstOrDefault();
+
+                        var defaultResponse = new WareStockEquipmentResponse
+                        {
+                            ResourceId = resource?.ResourceId ?? Guid.Empty,
+                            EquipmentId = resource?.EquipmentId ?? Guid.Empty,
+                            EquipmentCode = resource?.Equipment?.EquipmentCode,
+                            EquipmentName = resource?.Equipment?.EquipmentName,
+                            MaterialId = resource?.Equipment?.MaterialId,
+                            Material = existMaterial?.SubCategoryName,
+                            Usage = resource?.Equipment?.Usage,
+                            Warranty = resource?.Equipment?.Warranty,
+                            SizeUnitId = resource?.Equipment?.SizeUnitId,
+                            Size = resource?.Equipment?.Size,
+                            WeightUnitId = resource?.Equipment?.WeightUnitId,
+                            Weight = resource?.Equipment?.Weight,
+                            PurchaseDate = resource?.Equipment?.PurchaseDate,
+                            SpecQuantity = (resource?.PackageSize > 0)
+                                                ? $"{(int)(quantity / resource.PackageSize)} {package.SubCategoryName}" +
+                                                  $"{(quantity % resource.PackageSize > 0 ? $" (lẻ {quantity % resource.PackageSize} {unit.SubCategoryName})" : "")}"
+                                                : $"{(int)quantity} {unit.SubCategoryName}",
+                            UnitSpecification = $"{resource.PackageSize} {unit.SubCategoryName}/{package.SubCategoryName}",
+                            CurrentSupplierId = null,
+                            CurrentSupplierName = "Không rõ nguồn gốc",
+                            SupplierName = resourceSuppliers.Count > 0 ? $"Có {resourceSuppliers.Count} nhà cung cấp" : "Không rõ nguồn gốc",
+                            SuppliersName = resourceSuppliers
+                                    .Where(rs => rs.Supplier != null)
+                                    .GroupBy(rs => rs?.Supplier?.SupplierId)
+                                    .Select(g =>
+                                    {
+                                        var first = g.First();
+                                        var matchedResourceSupplier = g.FirstOrDefault(x =>
+                                            x.ResourceId == resource?.ResourceId &&
+                                            x.SupplierId == x.Supplier?.SupplierId);
+
+                                        return new SupplierResponse
+                                        {
+                                            SupplierId = first.Supplier?.SupplierId,
+                                            SupplierCode = first.Supplier?.SupplierCode,
+                                            SupplierName = first.Supplier?.SupplierName,
+                                            ResourceSupplierId = matchedResourceSupplier?.ResourceSupplierId
+                                        };
+                                    })
+                                    .ToList()
+                        };
+
+                        return new List<WareStockEquipmentResponse> { defaultResponse };
+                    }
+
+                    if (existResourceType.SubCategoryName == "medicine" && !resource.WareStocks.Any(x => x.SupplierId != null))
+                    {
+                        var existDisease = _unitOfWork.SubCategoryRepository.Get(filter: f => f.SubCategoryId.Equals(resource.Medicine.DiseaseId) && f.IsDeleted == false).FirstOrDefault();
+
+                        var defaultResponse = new WareStockMedicineResponse
+                        {
+                            ResourceId = resource?.ResourceId ?? Guid.Empty,
+                            MedicineId = resource?.MedicineId ?? Guid.Empty,
+                            MedicineCode = resource?.Medicine?.MedicineCode,
+                            MedicineName = resource?.Medicine?.MedicineName,
+                            Usage = resource?.Medicine?.Usage,
+                            DosageForm = resource?.Medicine?.DosageForm,
+                            StorageCondition = resource?.Medicine?.StorageCondition,
+                            DiseaseId = resource?.Medicine?.DiseaseId,
+                            Disease = existDisease?.SubCategoryName,
+                            ProductionDate = resource?.Medicine?.ProductionDate,
+                            ExpiryDate = resource?.Medicine?.ExpiryDate,
+                            SpecQuantity = (resource?.PackageSize > 0)
+                                                ? $"{(int)(quantity / resource.PackageSize)} {package.SubCategoryName}" +
+                                                  $"{(quantity % resource.PackageSize > 0 ? $" (lẻ {quantity % resource.PackageSize} {unit.SubCategoryName})" : "")}"
+                                                : $"{(int)quantity} {unit.SubCategoryName}",
+                            UnitSpecification = $"{resource.PackageSize} {unit.SubCategoryName}/{package.SubCategoryName}",
+                            CurrentSupplierId = null,
+                            CurrentSupplierName = "Không rõ nguồn gốc",
+                            SupplierName = resourceSuppliers.Count > 0 ? $"Có {resourceSuppliers.Count} nhà cung cấp" : "Không rõ nguồn gốc",
+                            SuppliersName = resourceSuppliers
+                                    .Where(rs => rs.Supplier != null)
+                                    .GroupBy(rs => rs?.Supplier?.SupplierId)
+                                    .Select(g =>
+                                    {
+                                        var first = g.First();
+                                        var matchedResourceSupplier = g.FirstOrDefault(x =>
+                                            x.ResourceId == resource?.ResourceId &&
+                                            x.SupplierId == x.Supplier?.SupplierId);
+
+                                        return new SupplierResponse
+                                        {
+                                            SupplierId = first.Supplier?.SupplierId,
+                                            SupplierCode = first.Supplier?.SupplierCode,
+                                            SupplierName = first.Supplier?.SupplierName,
+                                            ResourceSupplierId = matchedResourceSupplier?.ResourceSupplierId
+                                        };
+                                    })
+                                    .ToList()
+                        };
+
+                        return new List<WareStockMedicineResponse> { defaultResponse };
+                    }
+
+                    if (existResourceType.SubCategoryName == "breeding" && !resource.WareStocks.Any(x => x.SupplierId != null))
+                    {
+                        var existChickenTypeName = _unitOfWork.SubCategoryRepository.Get(filter: f => f.SubCategoryId.Equals(resource.Chicken.ChickenTypeId) && f.IsDeleted == false).FirstOrDefault();
+
+                        var defaultResponse = new WareStockChickenBreedingResponse
+                        {
+                            ResourceId = resource?.ResourceId ?? Guid.Empty,
+                            ChickenId = resource?.ChickenId ?? Guid.Empty,
+                            ChickenCode = resource?.Chicken?.ChickenCode,
+                            ChickenName = resource?.Chicken?.ChickenName,
+                            Description = resource?.Chicken?.Description,
+                            ChickenTypeName = existChickenTypeName?.SubCategoryName,
+                            SpecQuantity = (resource?.PackageSize > 0)
+                                                ? $"{(int)(quantity / resource.PackageSize)} {package.SubCategoryName}" +
+                                                  $"{(quantity % resource.PackageSize > 0 ? $" (lẻ {quantity % resource.PackageSize} {unit.SubCategoryName})" : "")}"
+                                                : $"{(int)quantity} {unit.SubCategoryName}",
+                            UnitSpecification = $"{resource.PackageSize} {unit.SubCategoryName}/{package.SubCategoryName}",
+                            CurrentSupplierId = null,
+                            CurrentSupplierName = "Không rõ nguồn gốc",
+                            SupplierName = resourceSuppliers.Count > 0 ? $"Có {resourceSuppliers.Count} nhà cung cấp" : "Không rõ nguồn gốc",
+                            SuppliersName = resourceSuppliers
+                                    .Where(rs => rs.Supplier != null)
+                                    .GroupBy(rs => rs?.Supplier?.SupplierId)
+                                    .Select(g =>
+                                    {
+                                        var first = g.First();
+                                        var matchedResourceSupplier = g.FirstOrDefault(x =>
+                                            x.ResourceId == resource?.ResourceId &&
+                                            x.SupplierId == x.Supplier?.SupplierId);
+
+                                        return new SupplierResponse
+                                        {
+                                            SupplierId = first.Supplier?.SupplierId,
+                                            SupplierCode = first.Supplier?.SupplierCode,
+                                            SupplierName = first.Supplier?.SupplierName,
+                                            ResourceSupplierId = matchedResourceSupplier?.ResourceSupplierId
+                                        };
+                                    })
+                                    .ToList()
+                        };
+
+                        return new List<WareStockChickenBreedingResponse> { defaultResponse };
+                    }
+
+                    if (existResourceType.SubCategoryName == "harvest_product" && !resource.WareStocks.Any(x => x.SupplierId != null))
+                    {
+                        var existHarvestProductType = _unitOfWork.SubCategoryRepository.Get(filter: f => f.SubCategoryId.Equals(resource.HarvestProduct.HarvestProductTypeId) && f.IsDeleted == false).FirstOrDefault();
+
+                        var defaultResponse = new WareStockHavestProductResponse
+                        {
+                            ResourceId = resource?.ResourceId ?? Guid.Empty,
+                            HarvestProductId = resource?.HarvestProductId ?? Guid.Empty,
+                            HarvestProductCode = resource?.HarvestProduct?.HarvestProductCode,
+                            HarvestProductName = resource?.HarvestProduct?.HarvestProductName,
+                            HarvestProductTypeId = existHarvestProductType?.SubCategoryId,
+                            HarvestProductTypeName = existHarvestProductType?.SubCategoryName,
+                            SpecQuantity = (resource?.PackageSize > 0)
+                                                ? $"{(int)(quantity / resource.PackageSize)} {package.SubCategoryName}" +
+                                                  $"{(quantity % resource.PackageSize > 0 ? $" (lẻ {quantity % resource.PackageSize} {unit.SubCategoryName})" : "")}"
+                                                : $"{(int)quantity} {unit.SubCategoryName}",
+                            UnitSpecification = $"{resource.PackageSize} {unit.SubCategoryName}/{package.SubCategoryName}",
+                            CurrentSupplierId = null,
+                            CurrentSupplierName = "Không rõ nguồn gốc",
+                            SupplierName = resourceSuppliers.Count > 0 ? $"Có {resourceSuppliers.Count} nhà cung cấp" : "Không rõ nguồn gốc",
+                            SuppliersName = resourceSuppliers
+                                    .Where(rs => rs.Supplier != null)
+                                    .GroupBy(rs => rs?.Supplier?.SupplierId)
+                                    .Select(g =>
+                                    {
+                                        var first = g.First();
+                                        var matchedResourceSupplier = g.FirstOrDefault(x =>
+                                            x.ResourceId == resource?.ResourceId &&
+                                            x.SupplierId == x.Supplier?.SupplierId);
+
+                                        return new SupplierResponse
+                                        {
+                                            SupplierId = first.Supplier?.SupplierId,
+                                            SupplierCode = first.Supplier?.SupplierCode,
+                                            SupplierName = first.Supplier?.SupplierName,
+                                            ResourceSupplierId = matchedResourceSupplier?.ResourceSupplierId
+                                        };
+                                    })
+                                    .ToList()
+                        };
+
+                        return new List<WareStockHavestProductResponse> { defaultResponse };
+                    }
+                }
+
                 var result = resourceSuppliers
                     .Where(rs => rs.Supplier != null)
                     .Select(rs =>
